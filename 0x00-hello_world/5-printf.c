2db20a8ff1db30c3208e6c2020492e06b984a91e
Lydiah6470 <starlight141524@gmail.com>
2022-12-08 04:55:03 -0800

    prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include < stdio.h>
+/**
+ * Description: main- Prints a  string to stdout.
+ * Return: 0 if succes.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
