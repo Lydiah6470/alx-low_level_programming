f1678174f724eba1a01f9503e1782ef069bdc194
Lydiah6470 <starlight141524@gmail.com>
2022-12-08 05:28:45 -0800

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main- prints string to stdout.
- * return: 0 if success
+ * Return: 0 if success
  */
 int main(void)
 {
-	write(2, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (0);
 }
