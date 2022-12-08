d7cf37a0ecfa265cc5e91ed7dfda4245d7b4bde0
Lydiah6470 <starlight141524@gmail.com>
2022-12-08 04:49:41 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * description: main- printing. 
- * print an output using puts.
+ * Description: main -Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
