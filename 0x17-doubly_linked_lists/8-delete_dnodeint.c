0faad2630eadbd1532997ef54a7777c9bf8c5c54
Lydiah Kiboi <118690917+Lydiah6470@users.noreply.github.com>
2023-03-16 22:16:47 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,50 +1,40 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at idx of a double linked list
- * @head: header of double linked list
- * @index: index of the node, starting from 0
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_dnodeint_at_index - deletes a node at a specific index
+ * @head: double pointer to the linked list
+ * @index: index at which to delete node
+ *
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *pre, *headcopy = *head;
+	dlistint_t *current;
 	unsigned int i;
 
-	while (headcopy != NULL && headcopy->prev != NULL)
-	{
-		headcopy = headcopy->prev;
-		*head = (*head)->prev;
-	}
-	if (headcopy != NULL && index != 0)
-	{
-		for (i = 0; i < index && headcopy != NULL; i++)
-		{
-			pre = headcopy;
-			headcopy = headcopy->next;
-		}
-		if (headcopy != NULL)
-		{
-			pre->next = headcopy->next;
-			if (pre->next != NULL)
-				headcopy->next->prev = pre;
-			free(headcopy);
-			return (1);
-		}
+	if (head == NULL || *head == NULL)
 		return (-1);
-	}
-	if (headcopy != NULL && index == 0)
+	current = *head;
+	if (index == 0)
 	{
-		pre = headcopy->next;
-		if (pre == NULL)
-			*head = NULL;
-		else
+		*head = current->next;
+		if (current->next != NULL)
 		{
-			pre->prev = NULL;
-			free(headcopy);
-			*head = pre;
+			current->next->prev = NULL;
 		}
+		free(current);
 		return (1);
 	}
-	return (-1);
+	for (i = 0; i < index; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
 }
